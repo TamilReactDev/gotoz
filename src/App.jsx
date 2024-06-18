25068b9cff027e7ff6f74e9906df6eb73b38dde4
TamilReactDev <levinhosmall@gmail.com>
2024-06-18 10:41:22 +0530

    successfully added

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import styles from "./style";
 
 
 import { Canvas } from '@react-three/fiber'
-import React, { Suspense, useRef } from 'react'
-import { useGLTF,PerspectiveCamera, AccumulativeShadows, RandomizedLight,OrbitControls,Stage, Environment, CameraControls } from '@react-three/drei'
+import React, { Suspense, useRef, useState } from 'react'
+import { useGLTF, PerspectiveCamera, AccumulativeShadows, RandomizedLight, OrbitControls, Stage, Environment, CameraControls } from '@react-three/drei'
 import { Monitor } from './component/Monitor';
 import AcquisitionCard from './component/AcquisitionCard';
 import Feature from './component/Feature';
@@ -22,24 +22,50 @@ function App() {
 
 
 	const ref = useRef();
+	const [value, setValue] = useState('3dTiles.png');
 
 	return (
 		<div className='w-full h-screen bg-yellow mx-auto'>
 			<div>
 				<Header />
 
-				
+
 			</div>
 			<section>
-			<Banner />
+				{/* <Banner /> */}
 			</section>
 			<section className='mt-16'>
-				<div className={`${styles.boxWidth} p-2` }>
+				<div className={`${styles.boxWidth} p-2`}>
 					<Feature />
 				</div>
 			</section>
-			
-			<Banner2 />
+			<div className='w-full h-full grid md:grid-cols-3 grid-cols-1 p-2 border-gray-200'>
+				<div className='w-full col-span-2 border-2 border-slate-200 h-[600px]'>
+					<Banner2 value={value} />
+				</div>
+				<div className='flex 2 flex-row gap-2 md:flex-col items-center justify-center'>
+					<div
+						className="block h-32 w-32 p-6 bg-white border border-gray-200 shadow-xl rounded-lg hover:bg-blue-100"
+						onClick={() => setValue('3dTiles.png')}
+					>
+						<img src="3dTiles.png" alt="tiles" className="h-full w-full object-cover" />
+					</div>
+					<div
+						className="block h-32 w-32 p-6 bg-white border border-gray-200 shadow-xl rounded-lg hover:bg-blue-100"
+						onClick={() => setValue('3dTiles1.jpg')}
+					>
+						<img src="3dTiles1.jpg" alt="tiles" className="h-full w-full object-cover" />
+					</div>
+					<div
+						className="block h-32 w-32 p-6 bg-white border border-gray-200 shadow-xl rounded-lg hover:bg-blue-100"
+						onClick={() => setValue('3dTiles2.jpg')}
+					>
+						<img src="3dTiles2.jpg" alt="tiles" className="h-full w-full object-cover" />
+					</div>
+				</div>
+			</div>
+
+
 
 		</div>
 	)
@@ -48,18 +74,18 @@ function App() {
 function Shoe(props) {
 	const { nodes, materials } = useGLTF('/shoe.gltf')
 	return (
-	  <group {...props} dispose={null}>
-		<mesh castShadow receiveShadow geometry={nodes.shoe.geometry} material={materials.laces} />
-		<mesh castShadow receiveShadow geometry={nodes.shoe_1.geometry} material={materials.mesh} />
-		<mesh castShadow receiveShadow geometry={nodes.shoe_2.geometry} material={materials.caps} />
-		<mesh castShadow receiveShadow geometry={nodes.shoe_3.geometry} material={materials.inner} />
-		<mesh castShadow receiveShadow geometry={nodes.shoe_4.geometry} material={materials.sole} />
-		<mesh castShadow receiveShadow geometry={nodes.shoe_5.geometry} material={materials.stripes} />
-		<mesh castShadow receiveShadow geometry={nodes.shoe_6.geometry} material={materials.band} />
-		<mesh castShadow receiveShadow geometry={nodes.shoe_7.geometry} material={materials.patch} />
-	  </group>
+		<group {...props} dispose={null}>
+			<mesh castShadow receiveShadow geometry={nodes.shoe.geometry} material={materials.laces} />
+			<mesh castShadow receiveShadow geometry={nodes.shoe_1.geometry} material={materials.mesh} />
+			<mesh castShadow receiveShadow geometry={nodes.shoe_2.geometry} material={materials.caps} />
+			<mesh castShadow receiveShadow geometry={nodes.shoe_3.geometry} material={materials.inner} />
+			<mesh castShadow receiveShadow geometry={nodes.shoe_4.geometry} material={materials.sole} />
+			<mesh castShadow receiveShadow geometry={nodes.shoe_5.geometry} material={materials.stripes} />
+			<mesh castShadow receiveShadow geometry={nodes.shoe_6.geometry} material={materials.band} />
+			<mesh castShadow receiveShadow geometry={nodes.shoe_7.geometry} material={materials.patch} />
+		</group>
 	)
-  }
-  
+}
+
 
 export default App
--- src/component/Banner2.jsx
+++ src/component/Banner2.jsx
@@ -1,26 +1,76 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '@google/model-viewer';
 
-function Banner2() {
-  return (
+function Banner2({value}) {
+    const modelViewerRef = useRef(null);
+    // const [value,setValue] = useState('');
     
+    useEffect(() => {
+        const modelViewer = modelViewerRef.current;
     
-        <model-viewer style={{ width: '100%', height: '100%' }}  src="shoe.gltf"  ar ar-modes="scene-viewer webxr " ar-placement='floor' camera-controls tone-mapping="neutral" poster="poster.webp" shadow-intensity="1"  auto-rotate
+        const handleLoad = () => {
+          const material = modelViewer.model.materials[0];
+          console.log(material);
+    
+          const createAndApplyTexture = async (channel, event) => {
+            if (!event) {
+              material[channel].setTexture(null);
+            } else if (event) {
+              const texture = await modelViewer.createTexture(event);
+              material[channel]['baseColorTexture'].setTexture(null);
+              material[channel]['baseColorTexture'].setTexture(texture);
+            
+            }
+          }
+    
+          if (value) {
+            console.log(value)
+            createAndApplyTexture('pbrMetallicRoughness', value);
+          }
+        };
+    
+        modelViewer.addEventListener('load', handleLoad);
+    
+        // Trigger handleLoad if model is already loaded
+        if (modelViewer.model && modelViewer.model.materials.length > 0) {
+          handleLoad();
+        }
+    
+        return () => {
+          modelViewer.removeEventListener('load', handleLoad);
+        };
+      }, [value]);
+
+    return (
+
+        <>
+          <model-viewer ref={modelViewerRef} style={{ width: '100%', height: '100%' }} src="plain1Tile.glb" ar ar-modes="scene-viewer webxr " ar-placement='floor' camera-controls tone-mapping="neutral" poster="poster.webp"  auto-rotate
+        exposure="1"
+        
         >
-    <div class="progress-bar hide" slot="progress-bar">
-        <div class="update-bar"></div>
-    </div>
-    <button slot="ar-button" className='bg-red-400' id="ar-button">
-        View in your space
-    </button>
-    <div id="ar-prompt">
-        <img src="https://modelviewer.dev/shared-assets/icons/hand.png" />
-    </div>
-</model-viewer>
-    
+            <div class="progress-bar" slot="progress-bar">
+                <div class="update-bar"></div>
+            </div>
+            <button slot="ar-button" className='bg-red-400' id="ar-button">
+                View in your space
+            </button>
+           
+            <div id="ar-prompt">
+                <img src="https://modelviewer.dev/shared-assets/icons/hand.png" />
+            </div>
+        </model-viewer>
+         {/* <select name=""  onChange={(e) => setValue(e.target.value)}  id="">
+         <option value="kids.png">tile1</option>
+         <option value="ar.png">Tile2</option>
+         <option value="3dTiles.png">Tile3</option>
+     </select> */}
+        </>
+        
+        
 
 
-  );
+
+    );
 }
 
 export default Banner2;
